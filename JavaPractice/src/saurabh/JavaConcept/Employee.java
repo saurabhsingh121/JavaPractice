67732675430b0a93b6c18aad089dd0813480e77b
Saurabh <Saurabh@OmSai>
2018-06-21 01:38:09 +0530

    Studying the concept of creating objects through reflection https://programmingmitra.blogspot.com/2016/05/creating-objects-through-reflection-in-java-with-example.html

--- JavaPractice/src/saurabh/JavaConcept/Employee.java
+++ JavaPractice/src/saurabh/JavaConcept/Employee.java
@@ -5,7 +5,12 @@ public class Employee {
 	private String name;
 	private int age;
 	private String city;
-	
+	//This no-arg constructor was created to create instance of it while providing only
+	//package structure name and I was testing it on saurabh.JavaConcept.ClassForNameTest Java class
+	//Class.newInstance() only creates object of class when it has no-arg constructor
+	public Employee() {
+		
+	}
 	public Employee(int id, String name, int age, String city) {
 		this.id = id;
 		this.name = name;
